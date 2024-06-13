778a6dcbc57a2f6864110007f014dbd06ac95d59
Jens Reidel <adrian@travitia.xyz>
2024-06-13 04:00:23 +0200

    input: touchscreen: xiaomi: Avoid NPE if getModeValue is not registered
    Change-Id: Ia7a70e1823aa36039634bafa8dcd8994139bed06
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- drivers/input/touchscreen/xiaomi/xiaomi_touch.c
+++ drivers/input/touchscreen/xiaomi/xiaomi_touch.c
@@ -1049,7 +1049,7 @@ static ssize_t gesture_single_tap_enabled_store(struct device *dev,
 	}
 	touch_data = touch_pdata->touch_data[0];
 
-	if (!touch_data->setModeValue)
+	if (!touch_data->getModeValue || !touch_data->setModeValue)
 		return -ENOMEM;
 
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)
@@ -1115,7 +1115,7 @@ static ssize_t gesture_double_tap_enabled_store(struct device *dev,
 	}
 	touch_data = touch_pdata->touch_data[0];
 
-	if (!touch_data->setModeValue)
+	if (!touch_data->getModeValue || !touch_data->setModeValue)
 		return -ENOMEM;
 
 	if (sscanf(buf, "%d", &input) < 0 || input > 1)
