3fe0d2c9bbd400cac582762b85f268c6b1f7c056
Arian <arian.kulmer@web.de>
2024-07-24 23:42:40 +0200

    input: touchscreen: Create exported xiaomi touch header
    Change-Id: Ic7004aff9220e9b07a9c16708f79eebf2ea790d9
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- drivers/input/touchscreen/xiaomi/xiaomi_touch.h
+++ drivers/input/touchscreen/xiaomi/xiaomi_touch.h
@@ -27,14 +27,13 @@
 #include <linux/time.h>
 #include <linux/time64.h>
 
+#include <uapi/linux/xiaomi_touch.h>
+
 /*CUR,DEFAULT,MIN,MAX*/
 #define VALUE_TYPE_SIZE 6
 #define VALUE_GRIP_SIZE 9
-#define MAX_BUF_SIZE 256
 #define BTN_INFO 0x152
-#define MAX_TOUCH_ID 10
 #define RAW_BUF_NUM 4
-#define THP_CMD_BASE	1000
 
 
 enum suspend_state {
@@ -42,43 +41,6 @@ enum suspend_state {
 	XIAOMI_TOUCH_SUSPEND,
 	XIAOMI_TOUCH_LP1,
 	XIAOMI_TOUCH_LP2,
-};
-
-enum MODE_CMD {
-	SET_CUR_VALUE = 0,
-	GET_CUR_VALUE,
-	GET_DEF_VALUE,
-	GET_MIN_VALUE,
-	GET_MAX_VALUE,
-	GET_MODE_VALUE,
-	RESET_MODE,
-	SET_LONG_VALUE,
-};
-
-enum MODE_TYPE {
-	Touch_Game_Mode				= 0,
-	Touch_Active_MODE      		= 1,
-	Touch_UP_THRESHOLD			= 2,
-	Touch_Tolerance				= 3,
-	Touch_Aim_Sensitivity       = 4,
-	Touch_Tap_Stability         = 5,
-	Touch_Expert_Mode           = 6,
-	Touch_Edge_Filter      		= 7,
-	Touch_Panel_Orientation 	= 8,
-	Touch_Report_Rate      		= 9,
-	Touch_Fod_Enable       		= 10,
-	Touch_Aod_Enable       		= 11,
-	Touch_Resist_RF        		= 12,
-	Touch_Idle_Time        		= 13,
-	Touch_Doubletap_Mode   		= 14,
-	Touch_Grip_Mode        		= 15,
-	Touch_FodIcon_Enable   		= 16,
-	Touch_Nonui_Mode       		= 17,
-	Touch_Debug_Level      		= 18,
-	Touch_Power_Status     		= 19,
-	Touch_Singletap_Gesture,
-	Touch_Mode_NUM,
-	THP_LOCK_SCAN_MODE      	= THP_CMD_BASE + 0,
 };
 
 struct xiaomi_touch_interface {
