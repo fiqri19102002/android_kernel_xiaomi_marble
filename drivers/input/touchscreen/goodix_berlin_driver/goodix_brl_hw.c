Atom-X-Devs/android_kernel_xiaomi_sm7325

touch: goodix: Replacing disable_irq with disable_irq_nosync
disable_irq ensures existing instances of the IRQ handler have
completed before returning which may create a deadlock if the handler
doesn't find the resource.

Change-Id: I7010d2a415f97de5ece8119b855c169ffba665b7
Signed-off-by: Simranjeet Thind <quic_sthind@quicinc.com>
Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

goodix_berlin_driver/goodix_brl_hw.c
@@ -312,11 +312,11 @@ static int brl_irq_enbale(struct goodix_ts_core *cd, bool enable)
 	}
 
 	if (!enable && atomic_cmpxchg(&cd->irq_enabled, 1, 0)) {
-		disable_irq(cd->irq);
+		disable_irq_nosync(cd->irq);
 		ts_info("Irq disabled");
 		return 0;
 	}
-	ts_debug("warnning: irq deepth inbalance!");
+	ts_debug("warning: irq depth imbalance!");
 	return 0;
 }
 

fiqri19102002/android_kernel_xiaomi_marble

Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

drivers/input/touchscreen/goodix_berlin_driver/goodix_brl_hw.c
@@ -334,11 +334,11 @@ static int brl_irq_enbale(struct goodix_ts_core *cd, bool enable)
 	}
 
 	if (!enable && atomic_cmpxchg(&cd->irq_enabled, 1, 0)) {
-		disable_irq(cd->irq);
+		disable_irq_nosync(cd->irq);
 		ts_debug("Irq disabled");
 		return 0;
 	}
-	ts_info("warnning: irq deepth inbalance!");
+	ts_info("warning: irq depth imbalance!");
 	return 0;
 }
 